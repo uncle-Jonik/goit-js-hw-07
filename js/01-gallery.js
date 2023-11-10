31646fe28f2e3ce5618d2a75bb468d6256aec3c8
uncle-Jonik <Katsimon_Eu@outlook.com>
2023-11-10 14:26:33 +0200

    add 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ulContainer = document.querySelector('.gallery');
+ulContainer.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+ulContainer.addEventListener('click', handelClick);
+
+function createMarkup(arr) {
+  /*
+ *   Даний варіант розмітки робить посилання на картинці яке при кліку хоче завантажити картинку
+ *    <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+              />
+          </a>
+      </li>
+ *   
+*/
+
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `
+      <li class="gallery__item">
+
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+              />
+
+      </li>
+    `
+    )
+    .join('');
+}
+
+function handelClick(event) {
+  if (event.target === event.currentTarget) {
+    return;
+  }
+
+  const currentProductId = event.target.dataset.source;
+
+  const product = galleryItems.find(
+    ({ original: productId }) => productId === currentProductId
+  );
+
+  const instance = basicLightbox.create(`
+    <img src="${product.original}" width="800" height="600">
+`);
+
+  instance.show();
+}
