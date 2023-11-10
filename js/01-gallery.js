64ba407c0d64effa7b2a1c4d3d21e24748f99f43
uncle-Jonik <Katsimon_Eu@outlook.com>
2023-11-10 14:28:02 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -26,14 +26,14 @@ function createMarkup(arr) {
       ({ preview, original, description }) =>
         `
       <li class="gallery__item">
-
+          <a class="gallery__link" href="${original}">
             <img
               class="gallery__image"
               src="${preview}"
               data-source="${original}"
               alt="${description}"
               />
-
+          </a>
       </li>
     `
     )
